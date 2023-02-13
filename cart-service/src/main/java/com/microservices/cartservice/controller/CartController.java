3f0db339505b64230bc80ce2db6ebb39bf31b150
Dung <michealng385@gmail.com>
2023-02-13 15:14:44 +0700

    Init docker and k8s

--- cart-service/src/main/java/com/microservices/cartservice/controller/CartController.java
+++ cart-service/src/main/java/com/microservices/cartservice/controller/CartController.java
@@ -1,17 +1,35 @@
 package com.microservices.cartservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/cart")
 public class CartController {
-    @GetMapping
+    private final WebClient.Builder webClientBuilder;
+
+    @GetMapping("/test")
     @ResponseStatus(HttpStatus.OK)
     public String getStatus() {
+        return "Cart alive";
+    }
+
+    @GetMapping("/test-client")
+    @ResponseStatus(HttpStatus.OK)
+    public String getStatusFromOtherService() {
+        webClientBuilder.build().get()
+                .uri("http://product-service/api/product")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
         return "Cart alive";
     }
 }
