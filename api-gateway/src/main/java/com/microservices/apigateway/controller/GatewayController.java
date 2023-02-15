04f9873eb7a4068c7212273d9bfa338323e51a11
Dung <michealng385@gmail.com>
2023-02-15 09:28:41 +0700

    Update k8s

--- api-gateway/src/main/java/com/microservices/apigateway/controller/GatewayController.java
+++ api-gateway/src/main/java/com/microservices/apigateway/controller/GatewayController.java
@@ -1,17 +1,15 @@
-package com.microservices.orderservice.controller;
+package com.microservices.apigateway.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/order")
-public class OrderController {
-    @GetMapping
+public class GatewayController {
+    @GetMapping("/test")
     @ResponseStatus(HttpStatus.OK)
     public String getStatus() {
-        return "Order alive";
+        return "Gateway alive";
     }
 }
