88f2632d007688fdc838f154d7191cf50ebc6b06
khanhkhanh196 <48844793+khanhkhanh196@users.noreply.github.com>
2023-02-16 10:29:26 +0700

    config update

--- discovery-server/src/main/java/com/microservices/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/microservices/discoveryserver/config/SecurityConfig.java
@@ -25,8 +25,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().anyRequest()
-                .authenticated()
+                .authorizeRequests().anyRequest().permitAll()
+//                .authenticated()
                 .and()
                 .httpBasic();
     }
